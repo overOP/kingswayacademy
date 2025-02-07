afb9c79e1ad3862d30741f08a2ff56cd0719edbb
overOP <cgpradeepop@gmail.com>
2025-02-07 17:18:40 +0545

    some section and footer

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 function toggleNav() {
     document.getElementById("mobile-nav").classList.toggle("flex");
   }
-  
+  function toggleAnswer(id) {
+    const answer = document.getElementById(`answer${id}`);
+    answer.classList.toggle("hidden");
+}
+
+
+// Back to Top button
+const backToTopBtn = document.getElementById('back-to-top');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    backToTopBtn.style.display = 'block';
+  } else {
+    backToTopBtn.style.display = 'none';
+  }
+});
+
+backToTopBtn.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
